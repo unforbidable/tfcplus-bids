cc0ee9ddf023d0bbd75e64293e7c1801009b2fac
unforbidable <unforbidable@gmail.com>
2022-07-02 15:55:22 +0200

    Force client update when GUI is opened

--- src/main/java/com/unforbidable/tfc/bids/Blocks/BlockCrucible.java
+++ src/main/java/com/unforbidable/tfc/bids/Blocks/BlockCrucible.java
@@ -43,6 +43,9 @@ public abstract class BlockCrucible extends BlockContainer {
         if (!world.isRemote && world.getTileEntity(i, j, k) instanceof TileEntityCrucible) {
             TileEntityCrucible crucible = (TileEntityCrucible) world.getTileEntity(i, j, k);
             entityplayer.openGui(Bids.instance, crucible.getGui(), world, i, j, k);
+
+            // Force update when gui is displayed
+            world.markBlockForUpdate(i, j, k);
         }
         return true;
     }
