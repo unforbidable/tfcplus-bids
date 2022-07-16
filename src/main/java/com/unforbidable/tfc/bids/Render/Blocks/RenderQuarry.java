20a90715203d15904e4ef9b9580fc42c3558fefd
unforbidable <unforbidable@gmail.com>
2022-07-17 01:33:14 +0200

    Render quarry WAILA icon

--- src/main/java/com/unforbidable/tfc/bids/Render/Blocks/RenderQuarry.java
+++ src/main/java/com/unforbidable/tfc/bids/Render/Blocks/RenderQuarry.java
@@ -2,6 +2,8 @@ package com.unforbidable.tfc.bids.Render.Blocks;
 
 import java.util.Map;
 
+import org.lwjgl.opengl.GL11;
+
 import com.dunk.tfc.api.TFCBlocks;
 import com.unforbidable.tfc.bids.Bids;
 import com.unforbidable.tfc.bids.Core.Quarry.QuarryWedgeBounds;
@@ -11,6 +13,7 @@ import com.unforbidable.tfc.bids.TileEntities.TileEntityQuarry;
 import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.RenderBlocks;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.common.util.ForgeDirection;
 
@@ -18,12 +21,23 @@ public class RenderQuarry implements ISimpleBlockRenderingHandler {
 
     @Override
     public void renderInventoryBlock(Block block, int metadata, int modelId, RenderBlocks renderer) {
+        Block materialBlock = TFCBlocks.woodAxle;
+        ForgeDirection front = ForgeDirection.UP;
+
+        for (ForgeDirection edge : ForgeDirection.VALID_DIRECTIONS) {
+            if (edge != front && edge != front.getOpposite()) {
+                for (int i = 0; i < 4; i++) {
+                    QuarryWedgeBounds[] b = QuarryWedgeModeller.getEdgeWedgeBounds(front, edge);
+                    renderer.setRenderBounds(b[i].x1, b[i].y1, b[i].z1, b[i].x2, b[i].y2, b[i].z2);
+                    renderInvBlock(materialBlock, metadata, renderer);
+                }
+            }
+        }
     }
 
     @Override
     public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId,
             RenderBlocks renderer) {
-
         TileEntityQuarry quarry = (TileEntityQuarry) world.getTileEntity(x, y, z);
         ForgeDirection front = ForgeDirection.getOrientation(world.getBlockMetadata(x, y, z) & 7);
         ForgeDirection back = front.getOpposite();
@@ -59,12 +73,42 @@ public class RenderQuarry implements ISimpleBlockRenderingHandler {
 
     @Override
     public boolean shouldRender3DInInventory(int modelId) {
-        return false;
+        return true;
     }
 
     @Override
     public int getRenderId() {
         return 0;
+    }
+
+    public static void renderInvBlock(Block block, int m, RenderBlocks renderer) {
+        Tessellator var14 = Tessellator.instance;
+        GL11.glTranslatef(-0.5F, -0.5F, -0.5F);
+        var14.startDrawingQuads();
+        var14.setNormal(0.0F, -1.0F, 0.0F);
+        renderer.renderFaceYNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(0, m));
+        var14.draw();
+        var14.startDrawingQuads();
+        var14.setNormal(0.0F, 1.0F, 0.0F);
+        renderer.renderFaceYPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(1, m));
+        var14.draw();
+        var14.startDrawingQuads();
+        var14.setNormal(-1.0F, 0.0F, 0.0F);
+        renderer.renderFaceXNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(2, m));
+        var14.draw();
+        var14.startDrawingQuads();
+        var14.setNormal(0.0F, 0.0F, -1.0F);
+        renderer.renderFaceZNeg(block, 0.0D, 0.0D, 0.0D, block.getIcon(3, m));
+        var14.draw();
+        var14.startDrawingQuads();
+        var14.setNormal(-1.0F, 0.0F, 0.0F);
+        renderer.renderFaceXPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(4, m));
+        var14.draw();
+        var14.startDrawingQuads();
+        var14.setNormal(0.0F, 0.0F, 1.0F);
+        renderer.renderFaceZPos(block, 0.0D, 0.0D, 0.0D, block.getIcon(5, m));
+        var14.draw();
+        GL11.glTranslatef(0.5F, 0.5F, 0.5F);
     }
 
 }
