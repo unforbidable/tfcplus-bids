6c17302ec91f1a16a118e9903abfeda91544477e
unforbidable <unforbidable@gmail.com>
2022-08-08 13:28:24 +0200

    Firepit construction requires 2 rocks

--- src/main/java/com/unforbidable/tfc/bids/Core/Firepit/FirepitHelper.java
+++ src/main/java/com/unforbidable/tfc/bids/Core/Firepit/FirepitHelper.java
@@ -1,16 +1,22 @@
 package com.unforbidable.tfc.bids.Core.Firepit;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.dunk.tfc.api.TFCBlocks;
+import com.dunk.tfc.api.TFCItems;
 import com.unforbidable.tfc.bids.Bids;
 import com.unforbidable.tfc.bids.TileEntities.TileEntityNewFirepit;
 import com.unforbidable.tfc.bids.api.BidsBlocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 
@@ -20,25 +26,75 @@ public class FirepitHelper {
             int side) {
         if (!world.isRemote && side == 1 && world.isAirBlock(x, y + 1, z)
                 && isValidFirepitLocation(world, x, y, z)) {
-            world.setBlock(x, y + 1, z, BidsBlocks.newFirepit);
-            TileEntity te = world.getTileEntity(x, y + 1, z);
-            if (te instanceof TileEntityNewFirepit) {
-                TileEntityNewFirepit firepit = (TileEntityNewFirepit) te;
-                firepit.initWithKindling(itemStack, false);
-                itemStack.stackSize--;
+            final List<EntityItem> rocks = findLooseRocksAt(world, x, y, z);
 
-                world.markBlockForUpdate(x, y + 1, z);
-            } else {
-                // We get here when the firepit block
-                // fails to create a TileEntityNewFirepit
-                world.setBlockToAir(x, y + 1, z);
-                Bids.LOG.warn("Firepit block did not create TileEntityNewFirepit tile entity");
+            if (getEntityListTotalStackSize(rocks) > 1) {
+                world.setBlock(x, y + 1, z, BidsBlocks.newFirepit);
+                TileEntity te = world.getTileEntity(x, y + 1, z);
+                if (te instanceof TileEntityNewFirepit) {
+                    TileEntityNewFirepit firepit = (TileEntityNewFirepit) te;
+                    firepit.initWithKindling(itemStack, false);
+                    itemStack.stackSize--;
+
+                    consumeStackFromEntityList(rocks, 2);
+
+                    world.markBlockForUpdate(x, y + 1, z);
+                } else {
+                    // We get here when the firepit block
+                    // fails to create a TileEntityNewFirepit
+                    world.setBlockToAir(x, y + 1, z);
+                    Bids.LOG.warn("Firepit block did not create TileEntityNewFirepit tile entity");
+                }
             }
 
             return true;
         }
 
         return false;
+    }
+
+    @SuppressWarnings("unchecked")
+    private static List<EntityItem> findLooseRocksAt(World world, int x, int y, int z) {
+        final AxisAlignedBB bounds = AxisAlignedBB.getBoundingBox(x, y, z, x + 1, y + 1.1, z + 1);
+        final List<EntityItem> everything = (List<EntityItem>) world.getEntitiesWithinAABB(EntityItem.class, bounds);
+        final List<EntityItem> rocks = new ArrayList<EntityItem>();
+
+        for (EntityItem entityItem : everything) {
+            if (entityItem.getEntityItem().getItem() == TFCItems.looseRock) {
+                rocks.add(entityItem);
+            }
+        }
+
+        return rocks;
+    }
+
+    private static int getEntityListTotalStackSize(List<EntityItem> list) {
+        int count = 0;
+
+        for (EntityItem entityItem : list) {
+            final ItemStack is = entityItem.getEntityItem();
+            count += is.stackSize;
+        }
+
+        return count;
+    }
+
+    private static void consumeStackFromEntityList(List<EntityItem> list, int count) {
+        for (EntityItem entityItem : list) {
+            final ItemStack is = entityItem.getEntityItem();
+            final int canConsumeCount = Math.min(is.stackSize, count);
+
+            count -= canConsumeCount;
+            is.stackSize -= canConsumeCount;
+
+            if (is.stackSize == 0) {
+                entityItem.setDead();
+            }
+
+            if (count == 0) {
+                break;
+            }
+        }
     }
 
     private static boolean isValidFirepitLocation(World world, int x, int y, int z) {
