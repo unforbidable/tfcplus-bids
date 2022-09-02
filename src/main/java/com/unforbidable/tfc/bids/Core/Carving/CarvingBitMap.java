8ff195cf7c4c4c8aceaf28bae585328f608b6bdb
unforbidable <unforbidable@gmail.com>
2022-09-02 15:24:33 +0200

    Carving recipes match rotated pattern

--- src/main/java/com/unforbidable/tfc/bids/Core/Carving/CarvingBitMap.java
+++ src/main/java/com/unforbidable/tfc/bids/Core/Carving/CarvingBitMap.java
@@ -82,4 +82,55 @@ public class CarvingBitMap {
         }
     }
 
+    public CarvingBitMap rotate(int rotation) {
+        CarvingBitMap rotated = new CarvingBitMap(dimension);
+
+        for (int bitX = 0; bitX < dimension; bitX++) {
+            for (int bitY = 0; bitY < dimension; bitY++) {
+                for (int bitZ = 0; bitZ < dimension; bitZ++) {
+                    if (testBit(bitX, bitY, bitZ)) {
+                        rotated.setBit(getRotatedBit(bitX, bitY, bitZ, rotation));
+                    }
+                }
+            }
+        }
+
+        return rotated;
+    }
+
+    private CarvingBit getRotatedBit(int bitX, int bitY, int bitZ, int rotation) {
+        switch (rotation % 4) {
+            case 0:
+                return new CarvingBit(bitX, bitY, bitZ);
+
+            case 1:
+                return new CarvingBit(bitZ, bitY, dimension - bitX - 1);
+
+            case 2:
+                return new CarvingBit(dimension - bitX - 1, bitY, dimension - bitZ - 1);
+
+            case 3:
+            default:
+                return new CarvingBit(dimension - bitZ - 1, bitY, bitX);
+        }
+    }
+
+    public boolean equals(CarvingBitMap carvedBits) {
+        if (dimension != carvedBits.dimension) {
+            return false;
+        }
+
+        for (int bitX = 0; bitX < dimension; bitX++) {
+            for (int bitY = 0; bitY < dimension; bitY++) {
+                for (int bitZ = 0; bitZ < dimension; bitZ++) {
+                    if (testBit(bitX, bitY, bitZ) != carvedBits.testBit(bitX, bitY, bitZ)) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
 }
