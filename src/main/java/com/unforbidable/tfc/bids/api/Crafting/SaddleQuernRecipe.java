bc92253faf7db114eedc6ab63d7a2f310818bb65
unforbidable <unforbidable@gmail.com>
2023-04-17 22:59:24 +0200

    Ignore cooked food status in saddle quern recipes

--- src/main/java/com/unforbidable/tfc/bids/api/Crafting/SaddleQuernRecipe.java
+++ src/main/java/com/unforbidable/tfc/bids/api/Crafting/SaddleQuernRecipe.java
@@ -20,7 +20,27 @@ public class SaddleQuernRecipe {
         return itemStack.getItem() == input.getItem()
                 && itemStack.getItemDamage() == input.getItemDamage()
                 && (!(input.getItem() instanceof IFood)
-                        || Food.areEqual(ItemFoodTFC.createTag(input.copy()), itemStack));
+                        || foodMatches(itemStack));
+    }
+
+    private boolean foodMatches(ItemStack itemStack) {
+        ItemStack inputFood = ItemFoodTFC.createTag(input.copy());
+
+        if (Food.areEqual(inputFood, itemStack)) {
+            return true;
+        }
+
+        // Allow cooked - set both to 0 and compare
+        if (Food.getCooked(itemStack) > 0) {
+            ItemStack inputFoodCooked = inputFood.copy();
+            Food.setCooked(inputFoodCooked,0);
+            ItemStack itemStackCooked = itemStack.copy();
+            Food.setCooked(itemStackCooked,0);
+
+            return Food.areEqual(inputFoodCooked, itemStackCooked);
+        }
+
+        return false;
     }
 
     public ItemStack getInput() {
