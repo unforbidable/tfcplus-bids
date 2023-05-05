02ef26736bff8aafc6dc990832b2832d515b00ab
unforbidable <unforbidable@gmail.com>
2023-05-05 09:32:54 +0200

    Salt water recipes take 2 pieces of salt

--- src/main/java/com/unforbidable/tfc/bids/Core/RecipeSetup.java
+++ src/main/java/com/unforbidable/tfc/bids/Core/RecipeSetup.java
@@ -530,15 +530,15 @@ public class RecipeSetup {
         }
 
         GameRegistry.addShapelessRecipe(new ItemStack(BidsItems.saltWaterBottle),
-            new ItemStack(TFCItems.powder, 1, 9),
+            new ItemStack(TFCItems.powder, 1, 9), new ItemStack(TFCItems.powder, 1, 9),
             new ItemStack(TFCItems.waterBottle), new ItemStack(TFCItems.glassBottle));
 
         GameRegistry.addShapelessRecipe(new ItemStack(TFCItems.woodenBucketSaltWater),
-            new ItemStack(TFCItems.powder, 1, 9),
+            new ItemStack(TFCItems.powder, 1, 9), new ItemStack(TFCItems.powder, 1, 9),
             new ItemStack(TFCItems.woodenBucketWater), new ItemStack(TFCItems.woodenBucketEmpty));
 
         GameRegistry.addShapelessRecipe(new ItemStack(TFCItems.clayBucketSaltWater),
-            new ItemStack(TFCItems.powder, 1, 9),
+            new ItemStack(TFCItems.powder, 1, 9), new ItemStack(TFCItems.powder, 1, 9),
             new ItemStack(TFCItems.clayBucketWater), new ItemStack(TFCItems.clayBucketEmpty));
 
         registerBowlMixingRecipe(BidsItems.honeyWaterBottle, TFCItems.honeybowl, TFCItems.waterBottle, TFCItems.glassBottle, true);
