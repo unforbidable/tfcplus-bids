827c09b035eee75a9e6c584efb3deaee45977eb3
unforbidable <unforbidable@gmail.com>
2023-10-27 17:33:45 +0200

    Large bowl places cooking prep surface

--- src/main/java/com/unforbidable/tfc/bids/Items/ItemLargeBowl.java
+++ src/main/java/com/unforbidable/tfc/bids/Items/ItemLargeBowl.java
@@ -4,10 +4,14 @@ import com.dunk.tfc.api.TFCFluids;
 import com.unforbidable.tfc.bids.BidsCreativeTabs;
 import com.unforbidable.tfc.bids.Core.Drinks.FluidHelper;
 import com.unforbidable.tfc.bids.Core.Drinks.IWorldFluidFillable;
+import com.unforbidable.tfc.bids.api.BidsBlocks;
 import com.unforbidable.tfc.bids.api.BidsItems;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
+import net.minecraftforge.common.util.ForgeDirection;
 import net.minecraftforge.fluids.Fluid;
 
 public class ItemLargeBowl extends ItemGenericPottery implements IWorldFluidFillable {
@@ -40,4 +44,23 @@ public class ItemLargeBowl extends ItemGenericPottery implements IWorldFluidFill
 
         return null;
     }
+
+    @Override
+    public boolean onItemUse(ItemStack itemStack, EntityPlayer player, World world, int x, int y, int z, int side,
+                             float hitX, float hitY, float hitZ) {
+        if (!world.isRemote && !player.isSneaking()) {
+            if (world.isAirBlock(x, y + 1, z) && world.isSideSolid(x, y, z, ForgeDirection.UP)) {
+                Block block = world.getBlock(x, y, z);
+                if (block.getMaterial() == Material.rock || block.getMaterial() == Material.wood || block.getMaterial() == Material.iron) {
+                    world.setBlock(x, y + 1, z, BidsBlocks.cookingPrep);
+                    return true;
+                }
+            }
+
+            return false;
+        } else {
+            return super.onItemUse(itemStack, player, world, x, y, z, side, hitX, hitY, hitZ);
+        }
+    }
+
 }
