185c4c2044ec1b47cb2308899c89748a1ed6e5c9
unforbidable <unforbidable@gmail.com>
2025-06-03 09:43:26 +0200

    Straw nest has collision box without gaps

--- src/main/java/com/unforbidable/tfc/bids/Blocks/BlockStrawNest.java
+++ src/main/java/com/unforbidable/tfc/bids/Blocks/BlockStrawNest.java
@@ -9,11 +9,15 @@ import com.unforbidable.tfc.bids.api.BidsGui;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 public class BlockStrawNest extends BlockContainer {
 
@@ -23,6 +27,19 @@ public class BlockStrawNest extends BlockContainer {
         setHardness(1f);
         setCreativeTab(BidsCreativeTabs.bidsDefault);
         setBlockBounds(0.2f, 0, 0.2f, 0.8f, 0.3f, 0.8f);
+    }
+
+    @SuppressWarnings({"unchecked"})
+    @Override
+    public void addCollisionBoxesToList(World world, int x, int y, int z, AxisAlignedBB aabb, List list, Entity entity) {
+        // For some reason the nest has such a size that allows chicken to get stuck and take damage
+        // Change collision box so that no gaps exist between the nest and the neighboring blocks
+        AxisAlignedBB axisalignedbb1 = AxisAlignedBB.getBoundingBox(x, y, z, x + 1, y + 0.2, z + 1);
+
+        if (aabb.intersectsWith(axisalignedbb1))
+        {
+            list.add(axisalignedbb1);
+        }
     }
 
     @Override
