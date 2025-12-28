2593227dee26c48f3e2ccd0ca053c6dadb9d2db1
unforbidable <unforbidable@gmail.com>
2025-12-28 19:49:47 +0100

    Add heckling recipe scheme

---
+++ src/main/java/com/unforbidable/tfc/bids/api/Crafting/HecklingRecipe.java
@@ -0,0 +1,11 @@
+package com.unforbidable.tfc.bids.api.Crafting;
+
+import net.minecraft.item.ItemStack;
+
+public class HecklingRecipe extends HandworkRecipe {
+
+    public HecklingRecipe(ItemStack output, ItemStack input, int duration) {
+        super(output, input, duration);
+    }
+
+}
