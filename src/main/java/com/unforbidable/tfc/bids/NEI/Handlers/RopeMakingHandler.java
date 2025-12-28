28a263ebd3ede7386c0b85660f044b973f4f6c89
unforbidable <unforbidable@gmail.com>
2025-12-28 16:44:58 +0100

    Handwork recipes use a common NEI handler

--- src/main/java/com/unforbidable/tfc/bids/NEI/Handlers/RopeMakingHandler.java
+++ src/main/java/com/unforbidable/tfc/bids/NEI/Handlers/RopeMakingHandler.java
@@ -1,26 +1,17 @@
 package com.unforbidable.tfc.bids.NEI.Handlers;
 
-import codechicken.nei.PositionedStack;
-import codechicken.nei.recipe.TemplateRecipeHandler;
 import com.dunk.tfc.api.TFCItems;
 import com.unforbidable.tfc.bids.NEI.HandlerInfo;
-import com.unforbidable.tfc.bids.NEI.IHandlerInfoProvider;
-import com.unforbidable.tfc.bids.Tags;
 import com.unforbidable.tfc.bids.api.BidsItems;
+import com.unforbidable.tfc.bids.api.BidsOptions;
+import com.unforbidable.tfc.bids.api.Crafting.HandworkRecipe;
 import com.unforbidable.tfc.bids.api.Crafting.RopeMakingManager;
-import com.unforbidable.tfc.bids.api.Crafting.RopeMakingRecipe;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.StatCollector;
 
-import java.awt.*;
-
-public class RopeMakingHandler extends TemplateRecipeHandler implements IHandlerInfoProvider {
+public class RopeMakingHandler extends HandworkHandler {
 
     static final String HANDLER_ID = "ropemaking";
-
-    public static ResourceLocation guiTexture = new ResourceLocation(Tags.MOD_ID,
-            "textures/gui/nei/gui_ropemaking.png");
 
     @Override
     public String getRecipeName() {
@@ -28,27 +19,16 @@ public class RopeMakingHandler extends TemplateRecipeHandler implements IHandler
     }
 
     @Override
-    public String getGuiTexture() {
-        return guiTexture.toString();
-    }
-
-    @Override
     public String getOverlayIdentifier() {
         return HANDLER_ID;
     }
-
-    @Override
-    public void loadTransferRects() {
-        transferRects.add(new RecipeTransferRect(new Rectangle(71, 23, 24, 18), HANDLER_ID));
-    }
-
     @Override
     public void loadCraftingRecipes(String outputId, Object... results) {
         if (outputId.equals(HANDLER_ID) && getClass() == RopeMakingHandler.class) {
-            for (RopeMakingRecipe recipe : RopeMakingManager.getRecipes()) {
+            for (HandworkRecipe recipe : RopeMakingManager.getRecipes()) {
                 final ItemStack input = recipe.getInput();
                 final ItemStack result = recipe.getResult(input);
-                arecipes.add(new CachedRopeMakingRecipe(input, result));
+                arecipes.add(new CachedHandworkRecipe(input, result, recipe.getDuration() * BidsOptions.Crafting.ropeMakingDurationMultiplier));
             }
         } else {
             super.loadCraftingRecipes(outputId, results);
@@ -57,7 +37,7 @@ public class RopeMakingHandler extends TemplateRecipeHandler implements IHandler
 
     @Override
     public void loadCraftingRecipes(ItemStack output) {
-        for (RopeMakingRecipe recipe : RopeMakingManager.getRecipes()) {
+        for (HandworkRecipe recipe : RopeMakingManager.getRecipes()) {
             final ItemStack output2 = output.copy();
             output2.stackSize = 1;
             final ItemStack input = recipe.getInput();
@@ -65,18 +45,18 @@ public class RopeMakingHandler extends TemplateRecipeHandler implements IHandler
             final ItemStack result2 = result.copy();
             result2.stackSize = 1;
             if (ItemStack.areItemStacksEqual(result2, output2)) {
-                arecipes.add(new CachedRopeMakingRecipe(input, result));
+                arecipes.add(new CachedHandworkRecipe(input, result, recipe.getDuration() * BidsOptions.Crafting.ropeMakingDurationMultiplier));
             }
         }
     }
 
     @Override
     public void loadUsageRecipes(ItemStack ingredient) {
-        for (RopeMakingRecipe recipe : RopeMakingManager.getRecipes()) {
+        for (HandworkRecipe recipe : RopeMakingManager.getRecipes()) {
             if (recipe.matchesIngredient(ingredient)) {
                 final ItemStack input = recipe.getInput();
                 final ItemStack result = recipe.getResult(input);
-                arecipes.add(new CachedRopeMakingRecipe(input, result));
+                arecipes.add(new CachedHandworkRecipe(input, result, recipe.getDuration() * BidsOptions.Crafting.ropeMakingDurationMultiplier));
             }
         }
     }
@@ -86,28 +66,6 @@ public class RopeMakingHandler extends TemplateRecipeHandler implements IHandler
         HandlerInfo info = new HandlerInfo(TFCItems.rope);
         info.addCatalyst(BidsItems.primitiveRopeMaker);
         return info;
-    }
-
-    public class CachedRopeMakingRecipe extends CachedRecipe {
-
-        final ItemStack ingred;
-        final ItemStack result;
-
-        public CachedRopeMakingRecipe(ItemStack ingred, ItemStack result) {
-            this.ingred = ingred.copy();
-            this.result = result.copy();
-        }
-
-        @Override
-        public PositionedStack getResult() {
-            return new PositionedStack(result, 111, 24);
-        }
-
-        @Override
-        public PositionedStack getIngredient() {
-            return new PositionedStack(ingred, 39, 24);
-        }
-
     }
 
 }
--- src/main/java/com/unforbidable/tfc/bids/NEI/Handlers/SpinningHandler.java
+++ src/main/java/com/unforbidable/tfc/bids/NEI/Handlers/SpinningHandler.java
@@ -1,35 +1,21 @@
 package com.unforbidable.tfc.bids.NEI.Handlers;
 
-import codechicken.nei.PositionedStack;
-import codechicken.nei.recipe.TemplateRecipeHandler;
 import com.unforbidable.tfc.bids.NEI.HandlerInfo;
-import com.unforbidable.tfc.bids.NEI.IHandlerInfoProvider;
-import com.unforbidable.tfc.bids.Tags;
 import com.unforbidable.tfc.bids.api.BidsItems;
+import com.unforbidable.tfc.bids.api.BidsOptions;
+import com.unforbidable.tfc.bids.api.Crafting.HandworkRecipe;
 import com.unforbidable.tfc.bids.api.Crafting.SpinningManager;
-import com.unforbidable.tfc.bids.api.Crafting.SpinningRecipe;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.StatCollector;
 import net.minecraftforge.oredict.OreDictionary;
 
-import java.awt.*;
-
-public class SpinningHandler extends TemplateRecipeHandler implements IHandlerInfoProvider {
+public class SpinningHandler extends HandworkHandler {
 
     static final String HANDLER_ID = "spinning";
-
-    public static ResourceLocation guiTexture = new ResourceLocation(Tags.MOD_ID,
-            "textures/gui/nei/gui_spinning.png");
 
     @Override
     public String getRecipeName() {
         return StatCollector.translateToLocal("gui.Spinning");
-    }
-
-    @Override
-    public String getGuiTexture() {
-        return guiTexture.toString();
     }
 
     @Override
@@ -38,17 +24,12 @@ public class SpinningHandler extends TemplateRecipeHandler implements IHandlerIn
     }
 
     @Override
-    public void loadTransferRects() {
-        transferRects.add(new RecipeTransferRect(new Rectangle(71, 23, 24, 18), HANDLER_ID));
-    }
-
-    @Override
     public void loadCraftingRecipes(String outputId, Object... results) {
         if (outputId.equals(HANDLER_ID) && getClass() == SpinningHandler.class) {
-            for (SpinningRecipe recipe : SpinningManager.getRecipes()) {
+            for (HandworkRecipe recipe : SpinningManager.getRecipes()) {
                 final ItemStack input = recipe.getInput();
                 final ItemStack result = recipe.getResult(input);
-                arecipes.add(new CachedHandcraftRecipe(input, result));
+                arecipes.add(new HandworkHandler.CachedHandworkRecipe(input, result, recipe.getDuration() * BidsOptions.Crafting.spinningDurationMultiplier));
             }
         } else {
             super.loadCraftingRecipes(outputId, results);
@@ -57,7 +38,7 @@ public class SpinningHandler extends TemplateRecipeHandler implements IHandlerIn
 
     @Override
     public void loadCraftingRecipes(ItemStack output) {
-        for (SpinningRecipe recipe : SpinningManager.getRecipes()) {
+        for (HandworkRecipe recipe : SpinningManager.getRecipes()) {
             final ItemStack output2 = output.copy();
             output2.stackSize = 1;
             final ItemStack input = recipe.getInput();
@@ -65,18 +46,18 @@ public class SpinningHandler extends TemplateRecipeHandler implements IHandlerIn
             final ItemStack result2 = result.copy();
             result2.stackSize = 1;
             if (ItemStack.areItemStacksEqual(result2, output2)) {
-                arecipes.add(new CachedHandcraftRecipe(input, result));
+                arecipes.add(new HandworkHandler.CachedHandworkRecipe(input, result, recipe.getDuration() * BidsOptions.Crafting.spinningDurationMultiplier));
             }
         }
     }
 
     @Override
     public void loadUsageRecipes(ItemStack ingredient) {
-        for (SpinningRecipe recipe : SpinningManager.getRecipes()) {
+        for (HandworkRecipe recipe : SpinningManager.getRecipes()) {
             if (recipe.matchesIngredient(ingredient)) {
-                final ItemStack input = new ItemStack(ingredient.getItem(), 1, ingredient.getItemDamage());
+                final ItemStack input = recipe.getInput();
                 final ItemStack result = recipe.getResult(input);
-                arecipes.add(new CachedHandcraftRecipe(input, result));
+                arecipes.add(new HandworkHandler.CachedHandworkRecipe(input, result, recipe.getDuration() * BidsOptions.Crafting.spinningDurationMultiplier));
             }
         }
     }
@@ -88,28 +69,6 @@ public class SpinningHandler extends TemplateRecipeHandler implements IHandlerIn
             info.addCatalyst(is.getItem());
         }
         return info;
-    }
-
-    public class CachedHandcraftRecipe extends CachedRecipe {
-
-        final ItemStack ingred;
-        final ItemStack result;
-
-        public CachedHandcraftRecipe(ItemStack ingred, ItemStack result) {
-            this.ingred = ingred.copy();
-            this.result = result.copy();
-        }
-
-        @Override
-        public PositionedStack getResult() {
-            return new PositionedStack(result, 111, 24);
-        }
-
-        @Override
-        public PositionedStack getIngredient() {
-            return new PositionedStack(ingred, 39, 24);
-        }
-
     }
 
 }
