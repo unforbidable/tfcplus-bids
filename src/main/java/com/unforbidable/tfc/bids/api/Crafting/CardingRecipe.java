d10ffa3ded5cb368706b680cb494aee3890455e8
unforbidable <unforbidable@gmail.com>
2025-12-28 19:44:39 +0100

    Add carding recipe scheme

---
+++ src/main/java/com/unforbidable/tfc/bids/api/Crafting/CardingRecipe.java
@@ -0,0 +1,11 @@
+package com.unforbidable.tfc.bids.api.Crafting;
+
+import net.minecraft.item.ItemStack;
+
+public class CardingRecipe extends HandworkRecipe {
+
+    public CardingRecipe(ItemStack output, ItemStack input, int duration) {
+        super(output, input, duration);
+    }
+
+}
