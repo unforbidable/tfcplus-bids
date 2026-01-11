ce9dbe4c32acc2ce1c2e81b74a3ac6acb9f090a5
unforbidable <unforbidable@gmail.com>
2026-01-11 17:15:26 +0100

    Play sound on woodcarving action

--- src/main/java/com/unforbidable/tfc/bids/Containers/ContainerWoodworking.java
+++ src/main/java/com/unforbidable/tfc/bids/Containers/ContainerWoodworking.java
@@ -82,6 +82,9 @@ public class ContainerWoodworking extends ContainerTFC implements IMessageHandli
                 player.inventory.setItemStack(null);
             }
 
+            world.playSoundEffect(player.posX, player.posY, player.posZ, "dig.wood",
+                0.4F + (world.rand.nextFloat() / 2), 0.7F + world.rand.nextFloat());
+
             tryToMatchCutout();
         }
 
