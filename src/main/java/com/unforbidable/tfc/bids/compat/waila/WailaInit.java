d31ec8afa051ba475619c2b4b9531cd15c517cc0
unforbidable <unforbidable@gmail.com>
2026-07-19 20:39:54 +0200

    Clean up WAILA setup

--- src/main/java/com/unforbidable/tfc/bids/compat/waila/WailaInit.java
+++ src/main/java/com/unforbidable/tfc/bids/compat/waila/WailaInit.java
@@ -18,30 +18,6 @@ public class WailaInit extends Initializable {
 
         FMLInterModComms.sendMessage("Waila", "register", WailaDataProvider.class.getCanonicalName() + ".setup");
         FMLInterModComms.sendMessage("Waila", "register", WailaEntityProvider.class.getCanonicalName() + ".setup");
-
-        // TODO register providers for respective features
-
-        //WailaProvider.addProvider(new CrucibleProvider(), TileEntityCrucible.class);
-        //WailaProvider.addProvider(new FurnaceProvider(), TileEntityChimney.class, TEChimney.class);
-        //WailaProvider.addProvider(new QuarryProvider(), TileEntityQuarry.class);
-//        WailaProvider.addProvider(new CarvingProvider(), TileEntityCarving.class);
-//        WailaProvider.addProvider(new WoodPileProvider(), TileEntityWoodPile.class);
-//        WailaProvider.addProvider(new FirepitProvider(), TileEntityNewFirepit.class);
-//        WailaProvider.addProvider(new DryingRackProvider(), TileEntityDryingRack.class);
-//        WailaProvider.addProvider(new ChoppingBlockProvider(), TileEntityChoppingBlock.class);
-//        WailaProvider.addProvider(new SaddleQuernProvider(), TileEntitySaddleQuern.class);
-//        WailaProvider.addProvider(new ClayLampProvider(), TileEntityClayLamp.class);
-//        WailaProvider.addProvider(new GenericSoilProvider(), BlockAquifer.class);
-//        WailaProvider.addProvider(new CookingPotProvider(), TileEntityCookingPot.class);
-//        WailaProvider.addProvider(new ScrewPressBarrelProvider(), TileEntityScrewPressBarrel.class);
-//        WailaProvider.addProvider(new CrackedOreProvider(), BlockCrackedOre.class, BlockCrackedOre2.class, BlockCrackedOre3.class);
-//        WailaProvider.addProvider(new ProcessingSurfaceProvider(), TileEntityProcessingSurface.class);
-//        WailaProvider.addProvider(new DecorativeSurfaceProvider(), TileEntityDecorativeSurface.class);
-//        WailaProvider.addProvider(new SoakingSurfaceProvider(), TileEntitySoakingSurface.class);
-//        WailaProvider.addProvider(new DryingSurfaceProvider(), TileEntityDryingSurface.class);
-//
-//        WailaEntityProvider.addProvider(new AnimalMilkProvider(), IAnimal.class);
-
     }
 
 }
