1d08a044cf81cbf29fd67e084ebbd853c29d7a07
unforbidable <unforbidable@gmail.com>
2026-07-31 23:33:19 +0200

    Add common misc item

---
+++ src/main/java/com/unforbidable/tfc/bids/common/item/ItemCommonMisc.java
@@ -0,0 +1,113 @@
+package com.unforbidable.tfc.bids.common.item;
+
+import com.dunk.tfc.api.Enums.EnumItemReach;
+import com.dunk.tfc.api.Enums.EnumSize;
+import com.dunk.tfc.api.Enums.EnumWeight;
+import com.dunk.tfc.api.Interfaces.ISize;
+import com.unforbidable.tfc.bids.Tags;
+import com.unforbidable.tfc.bids.util.ItemHelper;
+import com.unforbidable.tfc.bids.util.accessor.ItemMetaNamesAccessor;
+import java.util.List;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
+
+public class ItemCommonMisc extends Item implements ISize, ItemMetaNamesAccessor {
+
+    protected String[] metaNames;
+    protected IIcon[] metaIcons;
+
+    public ItemCommonMisc() {
+        setMaxStackSize(64);
+    }
+
+    @Override
+    public Item setMetaNames(String[] names) {
+        this.metaNames = names;
+        return this;
+    }
+
+    @Override
+    public boolean getHasSubtypes() {
+        return metaNames != null;
+    }
+
+    @Override
+    public String[] getMetaNames() {
+        return metaNames;
+    }
+
+    @Override
+    public void registerIcons(IIconRegister registerer) {
+        if (metaNames != null) {
+            metaIcons = new IIcon[metaNames.length];
+            for (int i = 0; i < metaNames.length; i++) {
+                metaIcons[i] = registerer.registerIcon(Tags.MOD_ID + ":tools/"
+                    + this.getUnlocalizedName().replace("item.", "") + "." + metaNames[i]);
+            }
+        } else {
+            itemIcon = registerer.registerIcon(Tags.MOD_ID + ":tools/"
+                + this.getUnlocalizedName().replace("item.", ""));
+        }
+    }
+
+    @Override
+    public IIcon getIconFromDamage(int i) {
+        if (metaNames != null && i < metaNames.length && metaIcons[i] != null) {
+            return metaIcons[i];
+        } else {
+            return this.itemIcon;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void getSubItems(Item item, CreativeTabs tabs, List list) {
+        if (metaNames != null) {
+            for (int i = 0; i < this.metaNames.length; ++i) {
+                list.add(new ItemStack(this, 1, i));
+            }
+        } else {
+            list.add(new ItemStack(this, 1, 0));
+        }
+    }
+
+    @Override
+    public String getUnlocalizedName(ItemStack itemstack) {
+        if (metaNames != null) {
+            return this.getUnlocalizedName() + "." + metaNames[itemstack.getItemDamage()];
+        } else {
+            return super.getUnlocalizedName(itemstack);
+        }
+    }
+
+    @Override
+    public EnumItemReach getReach(ItemStack arg0) {
+        return EnumItemReach.SHORT;
+    }
+
+    @Override
+    public boolean canStack() {
+        return true;
+    }
+
+    @Override
+    public EnumSize getSize(ItemStack arg0) {
+        return EnumSize.TINY;
+    }
+
+    @Override
+    public EnumWeight getWeight(ItemStack arg0) {
+        return EnumWeight.LIGHT;
+    }
+
+    @SuppressWarnings({"unchecked"})
+    @Override
+    public void addInformation(ItemStack is, EntityPlayer player, List list, boolean arg3) {
+        ItemHelper.addSizeInformation(is, list);
+    }
+
+}
