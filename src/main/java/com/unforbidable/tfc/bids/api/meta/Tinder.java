d09ebb0495489247e9bf0a30ce3472a0cb174ec1
unforbidable <unforbidable@gmail.com>
2026-08-02 12:53:47 +0200

    More tinder

---
+++ src/main/java/com/unforbidable/tfc/bids/api/meta/Tinder.java
@@ -0,0 +1,13 @@
+package com.unforbidable.tfc.bids.api.meta;
+
+public interface Tinder {
+
+    int STRAW = 0;
+    int BIRCH_BARK = 1;
+    int BAST = 2;
+    int FLAX = 3;
+    int JUTE = 4;
+    int SISAL = 5;
+    int COTTON = 6;
+
+}
--- src/main/java/com/unforbidable/tfc/bids/features/crafting/firestarting/FireStarting.java
+++ src/main/java/com/unforbidable/tfc/bids/features/crafting/firestarting/FireStarting.java
@@ -2,6 +2,9 @@ package com.unforbidable.tfc.bids.features.crafting.firestarting;
 
 import com.dunk.tfc.api.TFCItems;
 import com.unforbidable.tfc.bids.api.BidsItems;
+import com.unforbidable.tfc.bids.api.meta.Tinder;
+import com.unforbidable.tfc.bids.api.meta.Tow;
+import com.unforbidable.tfc.bids.api.meta.Wood;
 import com.unforbidable.tfc.bids.api.names.ItemNames;
 import com.unforbidable.tfc.bids.core.crafting.MatchingRecipe;
 import com.unforbidable.tfc.bids.core.features.Feature;
@@ -47,7 +50,7 @@ public class FireStarting extends Feature {
                 .setMaxDamage(32));
 
         init.item(ItemNames.TINDER, ItemTinder::new)
-            .meta("Straw");
+            .meta("Straw", "Birch Bark", "Bast", "Flax", "Jute", "Sisal", "Cotton");
     }
 
     @SideOnly(Side.CLIENT)
@@ -67,6 +70,16 @@ public class FireStarting extends Feature {
         setup.event()
             .handler(new FireStartingEventHandler());
 
+        setup.ores("materialTinderGood")
+            .add(new ItemStack(BidsItems.tinder, 1, Tinder.BIRCH_BARK))
+            .add(new ItemStack(BidsItems.tinder, 1, Tinder.BAST))
+            .add(new ItemStack(BidsItems.tinder, 1, Tinder.FLAX))
+            .add(new ItemStack(BidsItems.tinder, 1, Tinder.SISAL));
+
+        setup.ores("materialTinderExcellent")
+            .add(new ItemStack(BidsItems.tinder, 1, Tinder.JUTE))
+            .add(new ItemStack(BidsItems.tinder, 1, Tinder.COTTON));
+
         setup.ores("stickWoodBundle")
             .add(TFCItems.stickBundle)
             .add(BidsItems.smallStickBundle)
@@ -80,8 +93,66 @@ public class FireStarting extends Feature {
             .addShaped(new ItemStack(BidsItems.bowDrillFirestarter), "SD", "B ",
                 'D', TFCItems.bow, 'S', "stickWood", 'B', "boardWood");
 
+        setup.ores("materialTinderStraw2")
+            .add(TFCItems.straw);
         setup.recipes()
-            .addShapeless(new ItemStack(BidsItems.tinder, 2, 0), TFCItems.straw);
+            .addShapeless(new ItemStack(BidsItems.tinder, 2, Tinder.STRAW), "materialTinderStraw2");
+
+        setup.ores("materialTinderBirchBark2")
+            .add(new ItemStack(BidsItems.bark, 1, Wood.BIRCH));
+        setup.recipes()
+            .addShapeless(new ItemStack(BidsItems.tinder, 2, Tinder.BIRCH_BARK), "materialTinderBirchBark2");
+
+        setup.ores("materialTinderBast1")
+            .add(BidsItems.barkCordage);
+        setup.recipes()
+            .addShapeless(new ItemStack(BidsItems.tinder, 1, Tinder.BAST), "materialTinderBast1");
+
+        setup.ores("materialTinderBast4")
+            .add(BidsItems.barkFiberCoarse, BidsItems.barkFiberSmooth, BidsItems.barkCordage);
+        setup.recipes()
+            .addShapeless(new ItemStack(BidsItems.tinder, 4, Tinder.BAST), "materialTinderBast4");
+
+        setup.ores("materialTinderFlax1")
+            .add(new ItemStack(BidsItems.tow, 1, Tow.FLAX));
+        setup.recipes()
+            .addShapeless(new ItemStack(BidsItems.tinder, 1, Tinder.FLAX), "materialTinderFlax1");
+
+        setup.ores("materialTinderFlax4")
+            .add(TFCItems.flaxFiber, BidsItems.flaxFiberCoarse, BidsItems.flaxFiberRefined);
+        setup.recipes()
+            .addShapeless(new ItemStack(BidsItems.tinder, 4, Tinder.FLAX), "materialTinderFlax4");
+
+        setup.ores("materialTinderJute1")
+            .add(BidsItems.juteTwine)
+            .add(new ItemStack(BidsItems.tow, 1, Tow.JUTE));
+        setup.recipes()
+            .addShapeless(new ItemStack(BidsItems.tinder, 1, Tinder.JUTE), "materialTinderJute1");
+
+        setup.ores("materialTinderJute4")
+            .add(BidsItems.juteFiberCoarse, BidsItems.juteFiberRefined);
+        setup.recipes()
+            .addShapeless(new ItemStack(BidsItems.tinder, 4, Tinder.JUTE), "materialTinderJute4");
+
+        setup.ores("materialTinderSisal1")
+            .add(BidsItems.sisalTwine);
+        setup.recipes()
+            .addShapeless(new ItemStack(BidsItems.tinder, 1, Tinder.SISAL), "materialTinderSisal1");
+
+        setup.ores("materialTinderSisal4")
+            .add(BidsItems.sisalFiberCoarse, BidsItems.sisalFiberRefined, BidsItems.sisalTwine);
+        setup.recipes()
+            .addShapeless(new ItemStack(BidsItems.tinder, 4, Tinder.SISAL), "materialTinderSisal4");
+
+        setup.ores("materialTinderCotton1")
+            .add(TFCItems.cottonYarn);
+        setup.recipes()
+            .addShapeless(new ItemStack(BidsItems.tinder, 1, Tinder.COTTON), "materialTinderCotton1");
+
+        setup.ores("materialTinderCotton4")
+            .add(TFCItems.cotton, BidsItems.cottonBollRefined, BidsItems.cottonFiberCoarse, BidsItems.cottonFiberRefined);
+        setup.recipes()
+            .addShapeless(new ItemStack(BidsItems.tinder, 4, Tinder.COTTON), "materialTinderCotton4");
 
         if (FireStartingConfig.replaceOriginalFirestarterRecipes) {
             setup.recipes()
