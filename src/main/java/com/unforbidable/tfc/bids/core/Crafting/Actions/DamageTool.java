294b6c1dac09b13cedbcd785770631d3b4834ea2
unforbidable <unforbidable@gmail.com>
2026-08-17 15:58:45 +0200

    Variable damage provider for damageTool recipe action

--- src/main/java/com/unforbidable/tfc/bids/core/Crafting/Actions/DamageTool.java
+++ src/main/java/com/unforbidable/tfc/bids/core/Crafting/Actions/DamageTool.java
@@ -5,6 +5,7 @@ import com.unforbidable.tfc.bids.core.crafting.CraftingContext;
 import cpw.mods.fml.common.gameevent.PlayerEvent.ItemCraftedEvent;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
 
@@ -12,10 +13,12 @@ public class DamageTool {
 
     protected final List<ItemStack> tools;
     protected final int damage;
+    protected final Function<ItemStack, Integer> damageProvider;
 
-    protected DamageTool(String oreName, int damage) {
+    protected DamageTool(String oreName, int damage, Function<ItemStack, Integer> damageProvider) {
         this.tools = OreDictionary.getOres(oreName, false);
         this.damage = damage;
+        this.damageProvider = damageProvider;
     }
 
     public static Consumer<CraftingContext> damageTool(String oreName) {
@@ -23,7 +26,12 @@ public class DamageTool {
     }
 
     public static Consumer<CraftingContext> damageTool(String oreName, int damage) {
-        return context -> new DamageTool(oreName, damage)
+        return context -> new DamageTool(oreName, damage, null)
+            .onItemCrafted(context);
+    }
+
+    public static Consumer<CraftingContext> damageTool(String oreName, Function<ItemStack, Integer> damageProvider) {
+        return context -> new DamageTool(oreName, 0, damageProvider)
             .onItemCrafted(context);
     }
 
@@ -32,21 +40,31 @@ public class DamageTool {
     }
 
     private void findAndDamageTools(ItemCraftedEvent event) {
+        int actualDamage = damage;
+
+        if (damageProvider != null) {
+            for (int i = 0; i < event.craftMatrix.getSizeInventory(); i++) {
+                if (event.craftMatrix.getStackInSlot(i) != null) {
+                    actualDamage += damageProvider.apply(event.craftMatrix.getStackInSlot(i));
+                }
+            }
+        }
+
         for (int i = 0; i < event.craftMatrix.getSizeInventory(); i++) {
             if (event.craftMatrix.getStackInSlot(i) != null) {
                 for (ItemStack is : tools) {
                     if (event.craftMatrix.getStackInSlot(i).getItem() == is.getItem()) {
                         Bids.LOG.debug("Found matching tool to be damaged: " + is.getDisplayName());
-                        damageToolInSlot(event, i);
+                        damageToolInSlot(event, i, actualDamage);
                     }
                 }
             }
         }
     }
 
-    private void damageToolInSlot(ItemCraftedEvent event, int i) {
+    private void damageToolInSlot(ItemCraftedEvent event, int i, int actualDamage) {
         ItemStack tool = event.craftMatrix.getStackInSlot(i);
-        if (damageToolItem(event, tool) && (tool.getItemDamage() != 0 || event.player.capabilities.isCreativeMode)) {
+        if (damageToolItem(event, tool, actualDamage) && (tool.getItemDamage() != 0 || event.player.capabilities.isCreativeMode)) {
             int stackSize = event.craftMatrix.getStackInSlot(i).stackSize;
             stackSize = Math.min(stackSize + 1, 2);
             event.craftMatrix.getStackInSlot(i).stackSize = stackSize;
@@ -55,8 +73,8 @@ public class DamageTool {
         }
     }
 
-    protected boolean damageToolItem(ItemCraftedEvent event, ItemStack tool) {
-        tool.damageItem(damage, event.player);
+    protected boolean damageToolItem(ItemCraftedEvent event, ItemStack tool, int actualDamage) {
+        tool.damageItem(actualDamage, event.player);
 
         return true;
     }
