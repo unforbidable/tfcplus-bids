7eb132e27526369543d1917c38ea7ba9baf7cfd2
unforbidable <unforbidable@gmail.com>
2026-08-17 15:59:10 +0200

    Add repair patch meta

---
+++ src/main/java/com/unforbidable/tfc/bids/compat/tfc/meta/RepairPatchMeta.java
@@ -0,0 +1,14 @@
+package com.unforbidable.tfc.bids.compat.tfc.meta;
+
+public interface RepairPatchMeta {
+
+    int WOOL = 0;
+    int SILK = 1;
+    int LEATHER = 2;
+    int LINEN = 3;
+    int WOLF_FUR = 4;
+    int BEAR_FUR = 5;
+    int COTTON = 6;
+    int FUR = 7;
+
+}
