538f72d1a34569eadbd2cf71a5afb69e257f5ceb
unforbidable <unforbidable@gmail.com>
2026-08-31 17:28:40 +0200

    Add stone meta

---
+++ src/main/java/com/unforbidable/tfc/bids/api/meta/StoneMeta.java
@@ -0,0 +1,27 @@
+package com.unforbidable.tfc.bids.api.meta;
+
+public interface StoneMeta {
+
+    int GRANITE = 0;
+    int DIORITE = 1;
+    int GABBRO = 2;
+    int SHALE = 3;
+    int CLAYSTONE = 4;
+    int ROCK_SALT = 5;
+    int LIMESTONE = 6;
+    int SANDSTONE = 7;
+    int DOLOMITE = 8;
+    int CHERT = 9;
+    int CHALK = 10;
+    int RHYOLITE = 11;
+    int BASALT = 12;
+    int ANDESITE = 13;
+    int DACITE = 14;
+    int QUARTZITE = 15;
+    int SLATE = 16;
+    int PHYLLITE = 17;
+    int SCHIST = 18;
+    int GNEISS = 19;
+    int MARBLE = 20;
+
+}
